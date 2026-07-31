40f7166ab8861a37eb3ca0b42b17d1ce38034b44
Christian Krueger <15275971+chriskruegerdev@users.noreply.github.com>
2026-07-31 12:06:56 +0200

    chore: update libraries and gradle config

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,33 +1,7 @@
-import org.gradle.kotlin.dsl.withType
-import org.jetbrains.kotlin.gradle.tasks.KotlinCompilationTask
-
 plugins {
     alias(libs.plugins.kotlin.serialization) apply false
     alias(libs.plugins.kotlin.multiplatform) apply false
     alias(libs.plugins.dokka) apply false
     alias(libs.plugins.ben.manes.versions) apply false
     alias(libs.plugins.maven.publish) apply false
-}
-
-buildscript {
-    repositories {
-        gradlePluginPortal()
-        google()
-        mavenCentral()
-    }
-}
-
-allprojects {
-    repositories {
-        google()
-        gradlePluginPortal()
-        mavenCentral()
-        mavenLocal()
-    }
-
-    tasks.withType<KotlinCompilationTask<*>>().configureEach {
-        compilerOptions {
-            allWarningsAsErrors.set(false)
-        }
-    }
 }
--- tmdb-api/src/commonMain/kotlin/app/moviebase/tmdb/TmdbClientConfig.kt
+++ tmdb-api/src/commonMain/kotlin/app/moviebase/tmdb/TmdbClientConfig.kt
@@ -1,6 +1,5 @@
 package app.moviebase.tmdb
 
-import app.moviebase.tmdb.core.TmdbDsl
 import io.ktor.client.*
 import io.ktor.client.engine.*
 import io.ktor.client.plugins.logging.*
